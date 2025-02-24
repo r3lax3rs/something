e8f60e8141edc61be121b7c920c71a1a2ad652a1
r3lax3rs <124855911+r3lax3rs@users.noreply.github.com>
2025-02-25 00:35:52 +0100

    Create ipsweep.sh

---
+++ ipsweep.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+if [ "$1" == "" ]
+then
+echo "You forgot an IP address!"
+echo "Example Syntax: ./ipsweep.sh 192.168.4"
+else
+
+for ip in `seq 1 254`; do
+ping -c 1 $1.$ip | grep "64 bytes" | awk '{print $4}' | tr -d ":" & #NOTE: when using & it will allow multiple instances at ones; when you replace it with ; it will do one per time
+done
+fi
