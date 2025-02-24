fba94bf9b207c06302f87458abe6b2c5bda9f030
r3lax3rs <124855911+r3lax3rs@users.noreply.github.com>
2025-02-25 00:37:12 +0100

    Update ipsweep.sh

--- ipsweep.sh
+++ ipsweep.sh
@@ -10,3 +10,4 @@ for ip in `seq 1 254`; do
 ping -c 1 $1.$ip | grep "64 bytes" | awk '{print $4}' | tr -d ":" & #NOTE: when using & it will allow multiple instances at ones; when you replace it with ; it will do one per time
 done
 fi
+# How to use: ./ipsweep.sh 192.168.x > ips.txt
